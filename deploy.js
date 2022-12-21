fdce302292df250de27191c1068bd37989402bdb
uv_2001 <65334844+yuvedha-21@users.noreply.github.com>
2022-12-21 14:18:53 +0530

    package installation

---
+++ deploy.js
@@ -0,0 +1,11 @@
+//to run this js file run ==> node deploy.js(deploy is a file name)
+//like remix first we need to compile our contracts so we need to install solc package ==> solidity compiler
+//to install this package we need  to install them using npm ==> node package manager
+//also we can use other package managers as well , like yarn package manager
+// to install yarn run ==> corepack enable
+//yarn will be enabled then, now, install solc using yarn run ==> yarn add solc
+//this would add some of the files to our system like - package.json which tells us about the dependencies we are using in this current project
+// node modules folder will have all the packages that are being installed in our project
+
+async function main() {}
+main;
